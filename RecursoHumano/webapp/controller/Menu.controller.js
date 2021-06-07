35b65eb000792ae27e6b8dcb232e31620190abd4
jaime-zamorano <yamian23@gmail.com>
2021-06-07 02:10:21 +0000

    se sube hasta createemployee falta custumizar

--- RecursoHumano/webapp/controller/Menu.controller.js
+++ RecursoHumano/webapp/controller/Menu.controller.js
@@ -1,15 +1,42 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
-	/**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-	function (Controller) {
-		"use strict";
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
 
-		return Controller.extend("Empleados.RecursoHumano.controller.Menu", {
-			onInit: function () {
+    function onInit() {
 
-			}
-		});
-	});
+    }
+
+    function onAfterRendering() {
+        // Error en el framework : Al agregar la dirección URL de "Firmar pedidos", el componente GenericTile debería navegar directamente a dicha URL,
+        // pero no funciona en la version 1.78. Por tanto, una solución  encontrada es eliminando la propiedad id del componente por jquery
+        var genericTileFirmarPedido = this.byId("linkFirmarPedido");
+        //Id del dom
+        var idGenericTileFirmarPedido = genericTileFirmarPedido.getId();
+        //Se vacia el id
+        //jQuery("#"+idGenericTileFirmarPedido)[0].id = "";
+    }
+
+    //Función al pulsar sobre el Tile "Crear empleado". Hace un routing a la vista "createEmployee"
+    function navCreateEmployee() {
+        //Se obtiene el conjuntos de routers del programa
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        //Se navega hacia el router "CreateEmployee"
+        oRouter.navTo("CreateEmployee", {}, false);
+    }
+
+    //Función al pulsar sobre el Tile "Ver empleados". Hace un routing a la vista "showEmployee"
+    function navShowEmployee() {
+        //Se obtiene el conjuntos de routers del programa
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        //Se navega hacia el router "CreateEmployee"
+        oRouter.navTo("ShowEmployee", {}, false);
+    }
+
+    return Controller.extend("Empleados.RecursoHumano.controller.Menu", {
+        onInit: onInit,
+        onAfterRendering: onAfterRendering,
+        navCreateEmployee: navCreateEmployee,
+        navShowEmployee: navShowEmployee
+    });
+});
